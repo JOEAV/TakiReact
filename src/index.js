a7de500ba1112c68ebcb655640443521f76d6537
Yoav Lahav <yoavl@wix.com>
2018-05-18 20:39:46 +0300

    delete" "

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'
-
-
-const container = document.getElementById('root');
-/* Directly adding react element */
-ReactDOM.render(<App />,container);
